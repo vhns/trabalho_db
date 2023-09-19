87be156366524160c6bb272c3505c946cb052d2e
Marco Aurelio Nissikawa Hisatomi <58121773+HissYuki@users.noreply.github.com>
2023-09-19 14:55:02 -0300

    Delete models directory

--- models/email.py
+++
@@ -1,9 +0,0 @@
-from models import Base, Person
-from sqlalchemy import VARCHAR, INTEGER, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
-
-class Email(Base):
-    __tablename__ = "email"
-    id: Mapped[int] = mapped_column(INTEGER,nullable=False,primary_key=True, autoincrement=True)
-    email: Mapped[str] = mapped_column(VARCHAR(100), nullable=False, unique=True)
-    person_id: Mapped[int] = mapped_column(INTEGER, ForeignKey(Person.id), nullable=False)
