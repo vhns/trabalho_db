ce7cfe9f50bcf94690df4569e03faa1cb70e4396
Vitor Hugo Nunes dos Santos <hugo.nunes@pucpr.edu.br>
2023-11-14 13:29:42 -0300

    Incluir minhas queries e mudar # para --
    Commit inclui 4 transações e mudar os comentários de # para --
    simplesmente pois ativa meus neurônios.

--- commit.sql
+++ commit.sql
@@ -1,6 +1,6 @@
 USE dc_db;
 
-# Inserção de novo funcionário
+-- Inserção de novo funcionário
 
 SET autocommit=FALSE;
 START TRANSACTION;
@@ -9,7 +9,7 @@ SET @employee_id := (SELECT LAST_INSERT_ID());
 INSERT INTO dc_db.employee VALUES(@person_id, 1, "Médico", "09:00:00", "18:00:00");
 COMMIT;
 
-# Inserção de nova bolsa de sangue de novo paciente
+-- Inserção de nova bolsa de sangue de novo paciente
 
 SET autocommit=FALSE;
 START TRANSACTION;
@@ -22,7 +22,7 @@ SET @blood_bag_id := (SELECT LAST_INSERT_ID());
 INSERT INTO dc_db.blood_storage VALUES(@blood_bag_id, 2);
 COMMIT;
 
-# Inseção de um recebedor novo
+-- Inseção de um recebedor novo
 
 SET autocommit=FALSE;
 START TRANSACTION;
@@ -33,7 +33,7 @@ SET @receive_date := (SELECT r.date_receive FROM dc_db.receiver r WHERE r.id_rec
 UPDATE dc_db.blood_bags b SET b.departure_date = @receive_date WHERE b.id_bags = 7;
 COMMIT;
 
-# Inserção de um pagamento novo
+-- Inserção de um pagamento novo
 
 SET autocommit=FALSE;
 START TRANSACTION;
@@ -43,3 +43,34 @@ INSERT INTO dc_db.desconts VALUES(NULL, 200.00, "Multa");
 SET @descount_id := (SELECT LAST_INSERT_ID());
 INSERT INTO dc_db.payment_desconts VALUES(@payment_id, @descount_id);
 COMMIT;
+
+-- Pegar informação dos sacos de sangue
+
+SELECT bb.id_bags, bb.blood_type, bb.entry_date, bb.departura_date, 
+       e.person_name AS employee_name, p.person_name AS patient_name
+FROM dc_db.blood_bags bb
+JOIN dc_db.employee e ON bb.id_employee = e.id_employee
+JOIN dc_db.pacient p ON bb.id_pacient = p.id_pacient;
+
+-- Juntar a informação do funcionário com o centro de doação
+
+SELECT emp.person_name AS employee_name, emp.e_function, dc.center_name
+FROM dc_db.employee emp
+JOIN dc_db.donation_center dc ON emp.id_donation_center = dc.id_center;
+
+-- Juntar informação do recebedor/paciente com o centro de doação
+
+SELECT r.person_name AS receiver_name, rc.date_receive, bb.blood_type, dc.center_name
+FROM dc_db.receiver r
+JOIN dc_db.receiver_center rc ON r.id_receiver = rc.id_receiver
+JOIN dc_db.blood_bags bb ON rc.id_bags = bb.id_bags
+JOIN dc_db.donation_center dc ON rc.id_center = dc.id_center;
+
+-- Junta informação de pagamento com os descontos
+SELECT p.id_payment, emp.person_name AS employee_name, p.payment_value, p.payment_date,
+       d.descont_description, d.descont_value
+FROM dc_db.payment p
+JOIN dc_db.employee emp ON p.id_employee = emp.id_employee
+LEFT JOIN dc_db.payment_desconts pd ON p.id_payment = pd.id_payment
+LEFT JOIN dc_db.desconts d ON pd.id_descont = d.id_descont;
+
